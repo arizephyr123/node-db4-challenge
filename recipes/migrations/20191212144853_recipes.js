077d38b27976f9738f550d06a02a050b3fa7a57b
Arielle Hollingsworth <arizephyr123@gmail.com>
2019-12-12 18:18:15 -0800

    Initial Commit, server up and running, seeds and migrations created but not functional yet

---
+++ recipes/migrations/20191212144853_recipes.js
@@ -0,0 +1,72 @@
+exports.up = function(knex) {
+  return (
+    knex.schema
+      // 01-Recipes Table
+      .createTable("recipes", tbl => {
+        //id
+        tbl.increments();
+        //recipe_name
+        tbl.string("recipe_name", 255).notNullable();
+      })
+      // 02-Ingredients Table
+      .createTable("ingredients", tbl => {
+        //id
+        tbl.increments();
+        //ingredient_name
+        tbl
+          .string("ingredient_name", 255)
+          .notNullable()
+          .unique();
+      })
+      // 03-Instructions Table
+      .createTable("instructions", tbl => {
+        //id
+        tbl.increments();
+        //step_number
+        tbl
+          .integer("step_number")
+          .notNullable()
+          .unsigned();
+        //instruction
+        tbl
+        .string("instruction")
+        .notNullable();
+        //recipe_id
+        tbl
+          .integer("recipe_id")
+          .notNullable()
+          .unsigned()
+          .references("id")
+          .inTable("recipes")
+          .onUpdate("CASCADE")
+          .onDelete("CASCADE");
+      })
+      //04-recipe_ingredients
+      .createTable("recipe_ingredients", tbl => {
+        //recipe_id
+        tbl
+        .integer("recipe_id")
+        .notNullable()
+        .unsigned()
+        .references("id")
+        .inTable("recipes")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+        //ingredient_id
+        tbl
+          .integer("ingredient_id")
+          .notNullable()
+          .unsigned()
+          .references("id")
+          .inTable("ingredients")
+          .onUpdate("CASCADE")
+          .onDelete("CASCADE");
+      })
+  );
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists("recipe_ingredients")
+    .dropTableIfExists("instructions");
+};
